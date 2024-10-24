3016b3a5de99c97b1db701b5e79418c515e2d375
Denilson <jordanpreseleccion@gmail.com>
2024-10-24 17:57:05 -0500

    Add funcionality to proyects resume in home

---
+++ src/data/ProyectsHome.ts
@@ -0,0 +1,88 @@
+import proyectImage from "@src/Images/Home/MMJ-Proyectn.png";
+
+export interface IProyectResume {
+  title: string;
+  description1: string;
+  description2: string;
+  achievements: string[];
+  image: ImageMetadata;
+}
+
+export const proyectsResume = [
+  {
+    title: "Sistema de Adquisición de datos Agrícola",
+    description1:
+      "Desarrollamos sistemas avanzados de adquisición de datos en tiempo real, utilizados en diferentes sectores industriales para optimizar procesos y mejorar la toma de decisiones.",
+    description2:
+      "Desarrollamos sistemas avanzados de adquisición de datos en tiempo real, utilizados en diferentes sectores industriales para optimizar procesos y mejorar la toma de decisiones.",
+    achievements: [
+      "Recopilación de datos en tiempo real",
+      "Análisis predictivo",
+      "Mejora en la toma de decisiones",
+      "Reducción de costos",
+      "Soluciones personalizadas según industria",
+    ],
+    image: proyectImage,
+  },
+
+  {
+    title: "Sistema de Adquisición de datos Mineria",
+    description1:
+      "Desarrollamos sistemas avanzados de adquisición de datos en tiempo real, utilizados en diferentes sectores industriales para optimizar procesos y mejorar la toma de decisiones.",
+    description2:
+      "Desarrollamos sistemas avanzados de adquisición de datos en tiempo real, utilizados en diferentes sectores industriales para optimizar procesos y mejorar la toma de decisiones.",
+    achievements: [
+      "Recopilación de datos en tiempo real",
+      "Análisis predictivo",
+      "Mejora en la toma de decisiones",
+      "Reducción de costos",
+      "Soluciones personalizadas según industria",
+    ],
+    image: proyectImage,
+  },
+  {
+    title: "Sistema de Adquisición de datos Industrial",
+    description1:
+      "Desarrollamos sistemas avanzados de adquisición de datos en tiempo real, utilizados en diferentes sectores industriales para optimizar procesos y mejorar la toma de decisiones.",
+    description2:
+      "Desarrollamos sistemas avanzados de adquisición de datos en tiempo real, utilizados en diferentes sectores industriales para optimizar procesos y mejorar la toma de decisiones.",
+    achievements: [
+      "Recopilación de datos en tiempo real",
+      "Análisis predictivo",
+      "Mejora en la toma de decisiones",
+      "Reducción de costos",
+      "Soluciones personalizadas según industria",
+    ],
+    image: proyectImage,
+  },
+  {
+    title: "Sistema de Adquisición de datos Tecnológica",
+    description1:
+      "Desarrollamos sistemas avanzados de adquisición de datos en tiempo real, utilizados en diferentes sectores industriales para optimizar procesos y mejorar la toma de decisiones.",
+    description2:
+      "Desarrollamos sistemas avanzados de adquisición de datos en tiempo real, utilizados en diferentes sectores industriales para optimizar procesos y mejorar la toma de decisiones.",
+    achievements: [
+      "Recopilación de datos en tiempo real",
+      "Análisis predictivo",
+      "Mejora en la toma de decisiones",
+      "Reducción de costos",
+      "Soluciones personalizadas según industria",
+    ],
+    image: proyectImage,
+  },
+  {
+    title: "Sistema de Adquisición de datos Administrativa",
+    description1:
+      "Desarrollamos sistemas avanzados de adquisición de datos en tiempo real, utilizados en diferentes sectores industriales para optimizar procesos y mejorar la toma de decisiones.",
+    description2:
+      "Desarrollamos sistemas avanzados de adquisición de datos en tiempo real, utilizados en diferentes sectores industriales para optimizar procesos y mejorar la toma de decisiones.",
+    achievements: [
+      "Recopilación de datos en tiempo real",
+      "Análisis predictivo",
+      "Mejora en la toma de decisiones",
+      "Reducción de costos",
+      "Soluciones personalizadas según industria",
+    ],
+    image: proyectImage,
+  },
+];
