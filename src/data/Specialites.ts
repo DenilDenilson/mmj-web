9a812ed6c9e0771702797bce4478c0fe80c8861c
Denilson <jordanpreseleccion@gmail.com>
2024-10-30 03:11:47 -0500

    Agregado secciones en embebidos

--- src/data/Specialites.ts
+++ src/data/Specialites.ts
@@ -1,13 +1,39 @@
 import bgEmbebido from "@src/Images/Carreras/HeroBg.avif";
+import bgCourseCard from "@src/Images/Carreras/BgCourseCard.png";
+import bgProyect from "@src/Images/Carreras/bgProject.png";
+
+export interface IProject {
+  bgProjectCard: ImageMetadata;
+  title: string;
+  description: string;
+  url: string;
+}
+
+export interface ICourse {
+  bgCourseCard: ImageMetadata;
+  startDate: string;
+  title: string;
+  description: string;
+  modality: string;
+  duration: number;
+  hours: number;
+  priceBase: number;
+  priceReal: number;
+}
 
 export interface ISpecialite {
   title: string;
   backgroundHeroImage: ImageMetadata;
   description: string;
   goals: string[];
+  studentProfile: string;
+  skillsTech: string[];
+  canPerform: string[];
+  courses: ICourse[];
+  projects: IProject[];
 }
 
-export const specialites = {
+export const specialites: { [key: string]: ISpecialite } = {
   embebido: {
     title: "Desarrollo de Software Embebido",
     backgroundHeroImage: bgEmbebido,
@@ -19,6 +45,83 @@ export const specialites = {
       "Dominar la programación en tiempo real y la optimización de recursos.",
       "Desarrollar aplicaciones robustas y escalables.",
       "Comprender los principios del desarrollo de software embebido.",
+    ],
+    studentProfile:
+      "Al concluir la especialidad de Desarrollo de Software Embebido, el estudiante será un profesional altamente capacitado con habilidades tanto teóricas como prácticas en el diseño, implementación y optimización de sistemas embebidos.",
+    skillsTech: [
+      "Dominio de Microcontroladores",
+      "Desarrollo de firmware con FreeRTOS",
+      "Programación de sistemas en tiempo real",
+      "Desarrollo de aplicaciones en C y C++",
+      "Conocimiento de sistemas embebidos",
+    ],
+    canPerform: [
+      "Ingeniero de software embebido",
+      "Desarrollador de sistemas embebidos",
+      "Ingeniero de sistemas en tiempo real",
+      "Desarrollador de automatización",
+      "Ingeniero de sistemas embebidos",
+    ],
+    courses: [
+      {
+        bgCourseCard: bgCourseCard,
+        startDate: "12 de febrero del 2025",
+        title: "Desarrollo de Software Embebido con Microcontroladores",
+        description:
+          "Aprende a desarrollar software para sistemas embebidos utilizando microcontroladores y herramientas de programación en tiempo real. Este curso incluye teoría y práctica con ejemplos aplicados a la industria. Este curso incluye teoría y práctica con ejemplos aplicados a la industria.",
+        modality: "virtual",
+        duration: 4,
+        hours: 40,
+        priceBase: 95.9,
+        priceReal: 70,
+      },
+      {
+        bgCourseCard: bgCourseCard,
+        startDate: "12 de febrero del 2025",
+        title: "Desarrollo de Software Embebido con Microcontroladores 2",
+        description:
+          "Aprende a desarrollar software para sistemas embebidos utilizando microcontroladores y herramientas de programación en tiempo real. Este curso incluye teoría y práctica con ejemplos aplicados a la industria. Este curso incluye teoría y práctica con ejemplos aplicados a la industria.",
+        modality: "virtual",
+        duration: 4,
+        hours: 40,
+        priceBase: 95.9,
+        priceReal: 70,
+      },
+      {
+        bgCourseCard: bgCourseCard,
+        startDate: "12 de febrero del 2025",
+        title: "Desarrollo de Software Embebido con Microcontroladores 3",
+        description:
+          "Aprende a desarrollar software para sistemas embebidos utilizando microcontroladores y herramientas de programación en tiempo real. Este curso incluye teoría y práctica con ejemplos aplicados a la industria. Este curso incluye teoría y práctica con ejemplos aplicados a la industria.",
+        modality: "virtual",
+        duration: 4,
+        hours: 40,
+        priceBase: 95.9,
+        priceReal: 70,
+      },
+    ],
+    projects: [
+      {
+        bgProjectCard: bgProyect,
+        title: "Diseña de un Sistema de Control para un Robot Móvil",
+        description:
+          "Desarrollarás un sistema de control embebido para electrodomésticos, integrando sensores y actuadores para mejorar su eficiencia y automatización.",
+        url: "#",
+      },
+      {
+        bgProjectCard: bgProyect,
+        title: "Diseña de un Sistema de Control para un Robot Móvil",
+        description:
+          "Desarrollarás un sistema de control embebido para electrodomésticos, integrando sensores y actuadores para mejorar su eficiencia y automatización.",
+        url: "#",
+      },
+      {
+        bgProjectCard: bgProyect,
+        title: "Diseña de un Sistema de Control para un Robot Móvil",
+        description:
+          "Desarrollarás un sistema de control embebido para electrodomésticos, integrando sensores y actuadores para mejorar su eficiencia y automatización.",
+        url: "#",
+      },
     ],
   },
 };
