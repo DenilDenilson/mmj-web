951e6d6d524eafc529b288e00638c0aec2d8b3bb
Denilson <jordanpreseleccion@gmail.com>
2024-11-10 02:28:50 -0500

    Second version home

--- src/data/ProyectsHome.ts
+++ src/data/ProyectsHome.ts
@@ -1,4 +1,8 @@
 import proyectImage from "@src/Images/Home/MMJ-Proyectn.png";
+import EnergiaProjectImage from "@src/Images/Home/EnergiaProjectImage.jpg";
+import AlertaProjectImage from "@src/Images/Home/AlertaProjectImage.jpg";
+import VaporProjectImage from "@src/Images/Home/VaporProjectImage.jpg";
+import AgricolaProjectImage from "@src/Images/Home/AgricolaProjectImage.jpg";
 
 export interface IProyectResume {
   title: string;
@@ -10,78 +14,78 @@ export interface IProyectResume {
 
 export const proyectsResume = [
   {
-    title: "Sistema de Adquisición de datos Agrícola",
+    title: "Sistema de Medición de energía",
     description1:
-      "Desarrollamos sistemas avanzados de adquisición de datos en tiempo real, utilizados en diferentes sectores industriales para optimizar procesos y mejorar la toma de decisiones.",
+      "Nuestro sistema de medición de energía se basa en el uso de dos equipos avanzados: el RS485 Modbus RTU carril DIN, un vatímetro digital LCD de 4 cables que mide corriente AC de hasta 100 amperios, voltaje AC, potencia activa, reactiva, aparente, factor de potencia y frecuencia en sistemas de 380V CA y 60Hz.",
     description2:
-      "Desarrollamos sistemas avanzados de adquisición de datos en tiempo real, utilizados en diferentes sectores industriales para optimizar procesos y mejorar la toma de decisiones.",
+      "Y el medidor de energía eléctrica de fase única DIN Rail Power Meter, que también monitorea parámetros eléctricos. Toda la data se envía a un software de adquisición de datos a través del protocolo Modbus RS485, donde se analizan todas las variables para un sistema de análisis de calidad de energía avanzado.",
     achievements: [
-      "Recopilación de datos en tiempo real",
-      "Análisis predictivo",
-      "Mejora en la toma de decisiones",
-      "Reducción de costos",
-      "Soluciones personalizadas según industria",
+      "Análisis avanzado de calidad de energía.",
+      "Medición precisa de corriente hasta 100A.",
+      "Adaptación a sistemas industriales de 380V CA.",
+      "Comunicación fiable vía Modbus RS485.",
+      "Diseño compacto para fácil instalación en carril DIN.",
     ],
-    image: proyectImage,
+    image: EnergiaProjectImage,
   },
 
   {
-    title: "Sistema de Adquisición de datos Mineria",
+    title: "Ecosistema para Gestión de Alertas",
     description1:
-      "Desarrollamos sistemas avanzados de adquisición de datos en tiempo real, utilizados en diferentes sectores industriales para optimizar procesos y mejorar la toma de decisiones.",
+      "El ecosistema de alertas integra un concentrador remoto con conectividad 4G LTE y GPS, enlazado con cinco botoneras inteligentes. Cada botonera tiene tres botones: Alerta, Precaución y Condiciones Normales.",
     description2:
-      "Desarrollamos sistemas avanzados de adquisición de datos en tiempo real, utilizados en diferentes sectores industriales para optimizar procesos y mejorar la toma de decisiones.",
+      "La comunicación entre el concentrador y las botoneras abarca hasta 200 metros por radio. Cuando se presiona un botón, la botonera notifica al concentrador, que transmite automáticamente la ubicación y estado a una plataforma web de gestión, facilitando la respuesta a emergencias.",
     achievements: [
-      "Recopilación de datos en tiempo real",
-      "Análisis predictivo",
-      "Mejora en la toma de decisiones",
-      "Reducción de costos",
-      "Soluciones personalizadas según industria",
+      "Envío automático de ubicación",
+      "Alertas con 4G LTE y GPS",
+      "Botoneras de tres niveles",
+      "Comunicación a 200 m por radio",
+      "Gestión rápida en plataforma web",
     ],
-    image: proyectImage,
+    image: AlertaProjectImage,
   },
   {
-    title: "Sistema de Adquisición de datos Industrial",
+    title: "Maquinaria Generadora de Vapor",
     description1:
-      "Desarrollamos sistemas avanzados de adquisición de datos en tiempo real, utilizados en diferentes sectores industriales para optimizar procesos y mejorar la toma de decisiones.",
+      "Nuestra máquina generadora de vapor, diseñada para la industria del calzado, produce vapor de alta presión y temperatura para procesos clave como el moldeado y tratamiento térmico. Equipado con un potente microcontrolador de 32 bits.",
     description2:
-      "Desarrollamos sistemas avanzados de adquisición de datos en tiempo real, utilizados en diferentes sectores industriales para optimizar procesos y mejorar la toma de decisiones.",
+      "Este sistema automatizado mejora la productividad, ahorra energía y agua, y requiere mínima intervención humana. Una pantalla HMI permite al operario configurar fácilmente los parámetros del proceso, garantizando la calidad y durabilidad de los productos finales.",
     achievements: [
-      "Recopilación de datos en tiempo real",
-      "Análisis predictivo",
-      "Mejora en la toma de decisiones",
-      "Reducción de costos",
-      "Soluciones personalizadas según industria",
+      "Generación de vapor de alta presión",
+      "Ahorro de energía y agua",
+      "Mejora en productividad del calzado",
+      "Configuración sencilla con HMI",
+      "Reducción de intervención humana y mejora de calidad",
     ],
-    image: proyectImage,
+    image: VaporProjectImage,
   },
   {
-    title: "Sistema de Adquisición de datos Tecnológica",
+    title: "Sistema de Monitoreo y Control Agrícola Inteligente",
     description1:
-      "Desarrollamos sistemas avanzados de adquisición de datos en tiempo real, utilizados en diferentes sectores industriales para optimizar procesos y mejorar la toma de decisiones.",
+      "Este software proporciona una plataforma integral para gestionar variables ambientales y de riego en múltiples zonas de cultivo. Diseñado para maximizar la eficiencia agrícola, permite monitorear en tiempo real datos meteorológicos como temperatura, humedad, irradiancia y velocidad del viento.",
     description2:
-      "Desarrollamos sistemas avanzados de adquisición de datos en tiempo real, utilizados en diferentes sectores industriales para optimizar procesos y mejorar la toma de decisiones.",
+      "Además, facilita la configuración de riego automatizado, adaptándose a las necesidades específicas de cada zona, y ofrece reportes detallados sobre el uso de agua y nutrientes. Con un sistema de alertas visuales, los usuarios pueden identificar rápidamente el estado de cada área, optimizando el desarrollo de los cultivos.",
     achievements: [
-      "Recopilación de datos en tiempo real",
-      "Análisis predictivo",
-      "Mejora en la toma de decisiones",
-      "Reducción de costos",
-      "Soluciones personalizadas según industria",
+      "Monitoreo climático en tiempo real",
+      "Riego automatizado por zonas",
+      "Uso eficiente de agua y nutrientes",
+      "Reportes detallados de condiciones",
+      "Alertas visuales para cada área",
     ],
-    image: proyectImage,
+    image: AgricolaProjectImage,
   },
   {
-    title: "Sistema de Adquisición de datos Administrativa",
+    title: "  Sistema de Seguimiento de Activos GNSS + LTE",
     description1:
-      "Desarrollamos sistemas avanzados de adquisición de datos en tiempo real, utilizados en diferentes sectores industriales para optimizar procesos y mejorar la toma de decisiones.",
+      "Este proyecto de rastreador GNSS + LTE está diseñado para monitorear activos valiosos, utilizando módulos compactos de GNSS y LTE para localizar y reportar la posición en tiempo real.",
     description2:
-      "Desarrollamos sistemas avanzados de adquisición de datos en tiempo real, utilizados en diferentes sectores industriales para optimizar procesos y mejorar la toma de decisiones.",
+      "Ideal para prevención de robos, seguimiento de vehículos en tránsito, y mantenimiento predictivo, incorpora tecnologías avanzadas de bajo consumo y un sistema de respaldo con batería para asegurar operatividad continua. La arquitectura compacta y robusta facilita su uso en entornos difíciles y asegura la protección de los activos en ubicaciones remotas.",
     achievements: [
-      "Recopilación de datos en tiempo real",
-      "Análisis predictivo",
-      "Mejora en la toma de decisiones",
-      "Reducción de costos",
-      "Soluciones personalizadas según industria",
+      "Monitoreo en tiempo real de activos valiosos",
+      "Comunicación confiable en ubicaciones remotas",
+      "Mantenimiento predictivo basado en ubicación",
+      "Diseño compacto y robusto para entornos difíciles",
+      "Tecnologías de bajo consumo con respaldo de batería",
     ],
     image: proyectImage,
   },
