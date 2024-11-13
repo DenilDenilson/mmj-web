ffb09f5b400713624ad954c31c8c60960d59507a
Denilson <jordanpreseleccion@gmail.com>
2024-11-13 01:39:17 -0500

    Added responsive and new content form in carrer section

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,25 @@
-import { defineCollection } from "astro:content";
+import { z, defineCollection } from "astro:content";
 
 // Definiendo mis colecciones
 const courseCollection = defineCollection({
   type: "data",
+  schema: z.object({
+    title: z.string(),
+    backgroundHeroImage: z.string(),
+    description: z.string(),
+    goals: z.array(z.string()),
+    studentProfile: z.string(),
+    skillsTech: z.array(z.string()),
+    canPerform: z.array(z.string()),
+    projects: z.array(
+      z.object({
+        bgProjectCard: z.string(),
+        title: z.string(),
+        description: z.string(),
+        url: z.string(),
+      }),
+    ),
+  }),
 });
 
 export const collections = {
