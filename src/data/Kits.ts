0f412e6e053f62ad9a4022fdb7517b6e1181fc92
Denilson <jordanpreseleccion@gmail.com>
2025-07-01 00:59:46 -0500

    Agrgado posters y kits

--- src/data/Kits.ts
+++ src/data/Kits.ts
@@ -4,6 +4,13 @@
 import KitAnsiC from "@src/Images/Carreras/Embebido/ANCICv2.webp";
 import KitPIC from "@src/Images/Carreras/Embebido/KitPIC.webp";
 import KitRedes from "@src/Images/Carreras/Embebido/KitRedes.webp";
+import KitEsp32IoT from "@src/Images/Carreras/IoT/KitEsp32IoT.webp";
+import KitRedCelular from "@src/Images/Carreras/IoT/KitRedCelular.webp";
+import KitSocEsp32 from "@src/Images/Carreras/IoT/KitSocEsp32.webp";
+import KitPython from "@src/Images/Carreras/Software/KitPython.webp";
+import KitLabview from "@src/Images/Carreras/Software/KitLabview.webp";
+import KitDiseñoDigital from "@src/Images/Carreras/Hardware/KitDiseñoDigital.webp";
+import KitSistemasElectronicos from "@src/Images/Carreras/Hardware/KitSistemasElectronicos.webp";
 // import KitAnsiC from "@public/Images/KitAnsiC.webp";
 
 // Acá la URI define la key del objeto
@@ -16,4 +23,12 @@ export const kitsImages: kitImage = {
   "pic-wifi": KitPIC,
   "pic-asm": KitPIC,
   redes: KitRedes,
+  "esp32-arduino": KitEsp32IoT,
+  "esp32-c": KitSocEsp32,
+  "red-celular": KitRedCelular,
+  python: KitPython,
+  labview: KitLabview,
+  altium: KitSistemasElectronicos,
+  digital: KitDiseñoDigital,
+  "analogica-digitales": KitSistemasElectronicos,
 };
--- src/data/bgImages.ts
+++ src/data/bgImages.ts
@@ -5,8 +5,12 @@ import ThumbPicASM from "@src/Images/Carreras/Embebido/ThumbPicASM.jpg";
 import ThumbRedesIndustriales from "@src/Images/Carreras/Embebido/ThumbRedesIndustriales.jpg";
 import ThumbRedCelular from "@src/Images/Carreras/IoT/ThumbRedCelular.jpg";
 import ThumbEsp32C from "@src/Images/Carreras/IoT/ThumbEsp32C.jpg";
+import ThumbArduinoIoT from "@src/Images/Carreras/IoT/ThumbArduinoIoT.jpg";
 import ThumbLabview from "@src/Images/Carreras/Software/ThumbLabview.jpg";
 import ThumbPython from "@src/Images/Carreras/Software/ThumbPython.jpg";
+import ThumbAltium from "@src/Images/Carreras/Hardware/ThumbAltium.jpg";
+import ThumbDiseñoDigital from "@src/Images/Carreras/Hardware/ThumbDiseñoDigital.jpg";
+import ThumbSitemasElectronicos from "@src/Images/Carreras/Hardware/ThumbSistemasElectronicos.jpg";
 
 interface bgImage {
   [key: string]: ImageMetadata;
@@ -18,8 +22,12 @@ export const bgImages: bgImage = {
   "pic-wifi": ThumbPicWiFi,
   "pic-asm": ThumbPicASM,
   redes: ThumbRedesIndustriales,
-  redcelular: ThumbRedCelular,
-  esp32c: ThumbEsp32C,
+  "esp32-arduino": ThumbArduinoIoT,
+  "esp32-c": ThumbEsp32C,
+  "red-celular": ThumbRedCelular,
   labview: ThumbLabview,
   python: ThumbPython,
+  altium: ThumbAltium,
+  digital: ThumbDiseñoDigital,
+  "analogica-digitales": ThumbSitemasElectronicos,
 };
