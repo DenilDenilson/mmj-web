9d76703fcccc68d865031c2cf5519cb5247f8510
Denilson <93796694+DenilDenilson@users.noreply.github.com>
2025-11-13 21:19:23 -0500

    Add Astro configuration file with integrations
    Configured Astro project with Node adapter, Tailwind, and MDX integration.

---
+++ src/pages/astro.config.mjs
@@ -0,0 +1,30 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
+
+import tailwind from '@astrojs/tailwind';
+
+import mdx from '@astrojs/mdx';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
+  integrations: [tailwind(), mdx()],
+  server: {
+    port: 8090,
+    host: true
+  },
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp'
+    }
+  },
+  vite: {
+    build: {
+      assetsInlineLimit: 0
+    }
+  }
+});
